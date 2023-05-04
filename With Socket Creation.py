4930377b0f9d2bda89a6aac5d39ef622e377eba5
GentNW <89643048+GentNW@users.noreply.github.com>
2023-05-04 19:25:05 +0300

    Added videos

--- With Socket Creation.py
+++ With Socket Creation.py
@@ -2,9 +2,7 @@
 # coding: utf-8
 
 # In[1]:
-
-
-!pip install dollarpy
+! pip install dollarpy
 
 
 # In[2]:
@@ -32,18 +30,22 @@ soc.bind((hostname,port))
 
 
 # In[ ]:
-
-
-soc.listen(5)
-conn , addr = soc.accept()
-print("device connected")
+#soc.listen(5)
+#conn , addr = soc.accept()
+#print("device connected")
 
 # In[ ]:
 
 #testing the classification live
+
 def Classify(testpoint):
   reco= Recognizer(classes)
-  results = reco.Recognize(testpoint)
+  print(reco)
+  if reco == None:
+        return 0
+  else:
+    
+    results = reco.recognize(testpoint)
   return results
 #pth = ""#path of video 
 #points = getPoint(pth)
@@ -63,7 +65,7 @@ def live_cam():
   left_shoulder=[]
   points=[]
   # For webcam input:
-  cap = cv2.VideoCapture(1)
+  cap = cv2.VideoCapture(0)
   with mp_pose.Pose(
       min_detection_confidence=0.5,
       min_tracking_confidence=0.5) as pose:
@@ -85,17 +87,17 @@ def live_cam():
       image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
       if results.pose_landmarks :
                 right_shoulder.append(
-                    Point(results.pose_landmarks.landmarks[19].x,
-                          results.pose_landmarks.landmarks[17].y,
+                    Point(results.pose_landmarks.landmark[19].x,
+                          results.pose_landmarks.landmark[17].y,
                           1)
                 )
                 left_shoulder.append(
-                    Point(results.pose_landmarks.landmarks[20].x,
-                          results.pose_landmarks.landmarks[18].y,
+                    Point(results.pose_landmarks.landmark[20].x,
+                          results.pose_landmarks.landmark[18].y,
                           2)
                 )
       points = right_shoulder+left_shoulder
-      if len(points) >= 150 and len(points) > len(classes):
+      if len(points) >= 150 and len(points) < len(classes):
                       right_shoulder = []
                       left_shoulder = []
                       res = Classify(points)
@@ -105,16 +107,17 @@ def live_cam():
           results.pose_landmarks,
           mp_pose.POSE_CONNECTIONS,
           landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-      co = [results.pose_landmarks.landmarks[19].x,results.pose_landmarks.landmarks[19].y]
-      co = str(co)
-      msg=bytes(co,'utf-8')
-      conn.send(msg)
+      #co = [results.pose_landmarks.landmarks[19].x,results.pose_landmarks.landmarks[19].y]
+      #co = str(co)
+      #msg=bytes(co,'utf-8')
+      #conn.send(msg)
       # Flip the image horizontally for a selfie-view display.
       cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
       if cv2.waitKey(5) & 0xFF == 27:
+        cv2.destroyallwindows()
         break
+  cv2.destroyallwindows()
   cap.release()
-
 # In[ ]:
 def getPoint(vidPath):
   with mp_pose.Pose (min_detection_confidence=0.7,min_tracking_confidence=0.7) as pose :
@@ -136,13 +139,13 @@ def getPoint(vidPath):
             results = pose.process(cv2.flip(image,1))
             if results.pose_landmarks :
               right_shoulder.append(
-                  Point(results.pose_landmarks.landmarks[19].x,
-                        results.pose_landmarks.landmarks[17].y,
+                  Point(results.pose_landmarks.landmark[19].x,
+                        results.pose_landmarks.landmark[17].y,
                         1)
               )
               left_shoulder.append(
-                  Point(results.pose_landmarks.landmarks[20].x,
-                        results.pose_landmarks.landmarks[18].y,
+                  Point(results.pose_landmarks.landmark[20].x,
+                        results.pose_landmarks.landmark[18].y,
                         2)
               )
                
@@ -156,9 +159,56 @@ classes=[]
 
 #Setting up the templates for classification
 
-pth = ""#path of video 
+pth = "Video/trim 1"#path of video 
 points = getPoint(pth)
-tmp = Template("",points)
+tmp = Template("Open",points)
+classes.append(tmp)
+
+pth = "Video/trim 2"#path of video 
+points = getPoint(pth)
+tmp = Template("Open",points)
+classes.append(tmp)
+
+pth = "Video/trim 3"#path of video 
+points = getPoint(pth)
+tmp = Template("Open",points)
+classes.append(tmp)
+
+pth = "Video/trim 4"#path of video 
+points = getPoint(pth)
+tmp = Template("Open",points)
+classes.append(tmp)
+
+pth = "Video/trim 5"#path of video 
+points = getPoint(pth)
+tmp = Template("Open",points)
+classes.append(tmp)
+
+pth = "Video/trim 6"#path of video 
+points = getPoint(pth)
+tmp = Template("Close",points)
+classes.append(tmp)
+
+pth = "Video/trim 7"#path of video 
+points = getPoint(pth)
+tmp = Template("Close",points)
+classes.append(tmp)
+
+pth = "Video/trim 8"#path of video 
+points = getPoint(pth)
+tmp = Template("Close",points)
+classes.append(tmp)
+
+pth = "Video/trim 9"#path of video 
+points = getPoint(pth)
+tmp = Template("Close",points)
+classes.append(tmp)
+
+pth = "Video/trim 10"#path of video 
+points = getPoint(pth)
+tmp = Template("Close",points)
 classes.append(tmp)
 # In[ ]:
 live_cam()
+# In[ ]:
+print("saba7o")
